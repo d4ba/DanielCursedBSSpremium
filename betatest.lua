407fa28f57ab2a2feabe421c88c1b670cf39ebd7
Danielgamer9799 <110137940+danielgamer9799@users.noreply.github.com>
2023-06-06 16:02:53 -0400

    Update betatest.lua

--- betatest.lua
+++ betatest.lua
@@ -2,4 +2,6 @@ user = game:GetService("Players").LocalPlayer.Name
 whitelisted = loadstring(game:HttpGet("https://raw.githubusercontent.com/danielgamer9799/DanielCursedBSSpremium/main/whitelistedusers.lua?token=GHSAT0AAAAAACDFZVRBF2RVTTULV6RNFK42ZD7SA4Q"))()
 if table.find(whitelisted,user) then
     print("betatested")
+else
+    print("not whitelisted")
 end
